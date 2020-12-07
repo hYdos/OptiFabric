b48f087399ecbfab13320e8dc69275c691749edd
BluCobalt <luke@blucobalt.dev>
2020-12-06 18:03:54 -0800

    updates versions and suppresses as many warnings that i can
    Signed-off-by: Luke Schwager <luke@blucobalt.dev>

--- src/main/java/me/modmuss50/optifabric/mixin/MixinTitleScreen.java
+++ src/main/java/me/modmuss50/optifabric/mixin/MixinTitleScreen.java
@@ -3,35 +3,29 @@ package me.modmuss50.optifabric.mixin;
 import me.modmuss50.optifabric.mod.Optifabric;
 import me.modmuss50.optifabric.mod.OptifabricError;
 import me.modmuss50.optifabric.mod.OptifineVersion;
-
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.text.Text;
-import net.minecraft.util.Util;
-import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public abstract class MixinTitleScreen extends Screen {
+public class MixinTitleScreen extends Screen {
 
 	protected MixinTitleScreen() {
 		super();
 	}
 
-	@Inject(method = "init", at = @At("RETURN"))
+	@Inject(method = "init", at = @At("TAIL"))
 	private void init(CallbackInfo info) {
 		Optifabric.checkForErrors();
 	}
 
-	@Inject(method = "render", at = @At("RETURN"))
-	private void render(int int_1, int int_2, float float_1, CallbackInfo info) {
+	@Inject(method = "render", at = @At("TAIL"))
+	private void render(CallbackInfo info) {
 		if (!OptifabricError.hasError()) {
-			this.drawString(MinecraftClient.getInstance().textRenderer, OptifineVersion.version, 2, this.height - 20, 0xFFFFFFFF);
+			this.textRenderer.draw(OptifineVersion.version, 2, this.height - 20, 0xFFFFFFFF);
 		}
 	}
 }
